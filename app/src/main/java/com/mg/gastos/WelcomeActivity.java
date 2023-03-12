69eb8fbf73be44323b81a89c26a0eedd712e47ea
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-03-11 23:45:04 -0300

    MainActivity y redirección

--- app/src/main/java/com/mg/gastos/WelcomeActivity.java
+++ app/src/main/java/com/mg/gastos/WelcomeActivity.java
@@ -3,15 +3,32 @@ package com.mg.gastos;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        findViewById(R.id.btn_next);
+        setButtonAction();
     }
+
+    private void setButtonAction() {
+        Button button = findViewById(R.id.btn_next);
+        button.setOnClickListener(v -> {
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            v.startAnimation(shake);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            this.finish();
+            startActivity(intent);
+        });
+    }
+
 }
